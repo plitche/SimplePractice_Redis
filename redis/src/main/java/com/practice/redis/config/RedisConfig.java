16deeb2257fcd5d0f59841fdf267fd9cacd7e7d5
plitche <kysu728@naver.com>
2023-06-29 23:12:07 +0900

    redis basic setting

---
+++ redis/src/main/java/com/practice/redis/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.practice.redis.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        System.out.println(host);
+        System.out.println("port = " + port);
+        return new LettuceConnectionFactory(host, port);
+    }
+
+
+}
